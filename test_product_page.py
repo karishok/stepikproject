079d452bc1a1ae06d079109eebfd8677d2fe51a7
Karishok <babaliants@bk.ru>
2020-09-21 17:39:38 +0300

    check everything

--- test_product_page.py
+++ test_product_page.py
@@ -5,6 +5,7 @@ from .pages.basket_page import BasketPage
 from .pages.login_page import LoginPage
 from .pages.base_page import BasePage
 
+@pytest.mark.need_review
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
@@ -20,6 +21,7 @@ def test_guest_can_add_product_to_basket(browser, link):
     page = ProductPage(browser, link)
     page.open()  # открываем страницу
     page.add_to_basket()
+    time.sleep(0.5)
     page.solve_quiz_and_get_code()
     page.is_price_is_true()
     page.is_product_in_basket()
@@ -47,6 +49,7 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.add_to_basket()
     page.diapeared_success_message()
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     page = ProductPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
@@ -55,6 +58,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
 
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
@@ -95,6 +99,7 @@ class TestUserAddToBasketFromProductPage():
         page.open()
         page.should_not_be_success_message()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
         page = ProductPage(browser, link)
